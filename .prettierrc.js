a99f467551fe94c3b4b8386e5034fd05774371fa
Constantine Nazarenko <const@nazarenko.es>
2023-06-01 19:57:39 +0300

    add Loader; add prettier config

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  singleQuote: true,
+  arrowParens: 'always',
+  trailingComma: 'all',
+  endOfLine: 'lf',
+  printWidth: 120,
+  bracketSpacing: true,
+  tabWidth: 2,
+  useTabs: false,
+};
--- src/App/Loading/index.tsx
+++ src/App/Loading/index.tsx
@@ -1,5 +1,17 @@
-import React from "react";
-import "./styles.scss";
+import React from 'react';
+import './styles.scss';
 export function Loading() {
-  return <div className="loading-screen">🌀 Loading...</div>;
+  return (
+    <>
+      <div className="overlay" />
+      <div className="loader-ring">
+        <div className="filler">
+          <div className="rotate rotate-1" />
+          <div className="rotate rotate-2" />
+          <div className="rotate-shadow" />
+          <div className="scale" />
+        </div>
+      </div>
+    </>
+  );
 }
